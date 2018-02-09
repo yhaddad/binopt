c6f09c5dccffc3ae3cebe99095aabb5489ae6973
Yacine Haddad <yhaddad@cern.ch>
2018-02-09 18:23:51 +0100

    adding unnitest

--- tests/test_binopt.py
+++ tests/test_binopt.py
@@ -14,27 +14,44 @@ import unittest
 from contextlib import contextmanager
 from click.testing import CliRunner
 
-from binopt import binopt
-from binopt import cli
-
-
+import binopt
+import numpy  as np
+from scipy import special as sp
+from scipy.special import logit, expit
 
 class test_binopt(unittest.TestCase):
-
     def setUp(self):
         pass
 
     def tearDown(self):
-        pass
+        sevent = 1000
+        bevent = 10000
+        X = np.concatenate(
+            (
+                expit(np.random.normal(+2.0, 2.0, sevent)),
+                expit(np.random.normal(-0.5, 2.0, bevent))
+            )
+        )
+        Y = np.concatenate((
+            np.ones(sevent),
+            np.zeros(bevent)
+        ))
+        W = np.concatenate((np.ones(sevent), np.ones(bevent)))
+
+        binner = binopt.optimize_bin(
+            nbins=3, range=[0, 1],
+            drop_last_bin=True,
+            fix_upper=True,
+            fix_lower=False,
+            use_kde_density=True
+        )
+        opt = binner.fit(X, Y, sample_weights=W,
+                         method="Nelder-Mead",
+                         breg=None, fom="AMS2")
+        self.assertEqual(opt.x.shape[0], 3)
 
     def test_000_something(self):
         pass
 
-    def test_command_line_interface(self):
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'binopt.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+if __name__ == '__main__':
+    unittest.main()
